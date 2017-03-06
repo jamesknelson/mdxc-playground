51c6315be61c421315dc92fbf15049545f0f42da
jamesknelson <james@jamesknelson.com>
2017-03-06 23:38:49 +0900

    initial commit

---
+++ content/index.page.js
@@ -0,0 +1,8 @@
+export default {
+  wrapper: 'SiteWrapper',
+  title: "Welcome to Sitepack",
+  content: require('./home.md'),
+  children: [
+    require('./about.md'),
+  ]
+}
---
+++ source/components/Spinner.js
@@ -0,0 +1,11 @@
+import './Spinner.scss'
+import React from 'react'
+
+export default function Spinner() {
+  return (
+    <div className="Spinner">
+      <div className="Spinner-a"></div>
+      <div className="Spinner-b"></div>
+    </div>
+  )
+}
